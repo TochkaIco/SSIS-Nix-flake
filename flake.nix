dfaed606ed233c22593c675c645223ecf08667fb
Fedor Romanov <fedor1378romanov@gmail.com>
2026-07-23 19:04:02 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,25 +1,31 @@
 {
+  description = "Tochka's System Configuration";
 
-	description = "Tochka's System Configuration";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-		home-manager = {
-      			url = "github:nix-community/home-manager/release-26.05";
-      			inputs.nixpkgs.follows = "nixpkgs";
-    		};
-	};
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+        ];
+      };
 
-	outputs = { nixpkgs, home-manager, ... }: {
-		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
-			modules = [
-				./configuration.nix
-
-				home-manager.nixosModules.home-manager
-			];
-		};
-	};
-
+      homeConfigurations.tochka = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [ ./home.nix ];
+      };
+    };
 }
