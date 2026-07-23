ee488eb4404f2bff0ababc1574ed496249229871
Fedor Romanov <fedor1378romanov@gmail.com>
2026-07-23 18:22:01 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+
+	description = "Tochka's System Configuration";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+
+		home-manager = {
+      			url = "github:nix-community/home-manager/release-26.05";
+      			inputs.nixpkgs.follows = "nixpkgs";
+    		};
+	};
+
+	outputs = { nixpkgs, home-manager, ... }: {
+		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+			system = "x86_64-linux";
+			modules = [
+				./configuration.nix
+
+				home-manager.nixosModules.home-manager
+			];
+		};
+	};
+
+}
---
+++ home.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+  # Basic user information
+  home.username = "tochka";
+  home.homeDirectory = "/home/tochka";
+
+  home.stateVersion = "26.05";
+
+  # User-only packages
+  home.packages = with pkgs; [];
+
+  # Program configurations
+  programs.git = {
+    enable = true;
+    settings.user.name = "Fedor Romanov";
+    settings.user.email = "fedor1378romanov@gmail.com";
+  };
+}
